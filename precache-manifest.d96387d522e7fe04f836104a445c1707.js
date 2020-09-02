3a0516e6a8c63c9798c7ceece508172ec9864b56
Pratik9999 <40793069+Pratik9999@users.noreply.github.com>
2020-09-02 10:33:06 +0530

    Updates

---
+++ precache-manifest.d96387d522e7fe04f836104a445c1707.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579909a8f525ddc8306f069518082b07",
+    "url": "/awesomekittens/index.html"
+  },
+  {
+    "revision": "bfe1bde2fbc21e287cd6",
+    "url": "/awesomekittens/static/css/main.0be30dd0.chunk.css"
+  },
+  {
+    "revision": "cbd1e0295feedc1489fa",
+    "url": "/awesomekittens/static/js/2.dc550ced.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/awesomekittens/static/js/2.dc550ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfe1bde2fbc21e287cd6",
+    "url": "/awesomekittens/static/js/main.1d070c68.chunk.js"
+  },
+  {
+    "revision": "ff5e61c6c864cff0c667",
+    "url": "/awesomekittens/static/js/runtime-main.99eee63d.js"
+  }
+]);
